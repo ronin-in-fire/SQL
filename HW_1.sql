65066772d938d2eda6277b844fa7376810807b4a
Rustam Aflyatunov <137717457+ronin-in-fire@users.noreply.github.com>
2023-07-30 17:10:52 +0300

    HW_1.sql

--- HW_1.sql
+++ HW_1.sql
@@ -1,9 +1,8 @@
-/*       Таблица employees
-
-`1) Создать таблицу employees`
+/*	Таблица employees
+1) Создать таблицу employees
 - id. serial,  primary key,
 - employee_name. Varchar(50), not null
-`2) Наполнить таблицу employee 70 строками.`
+2) Наполнить таблицу employee 70 строками.
 */
 create table employees(
 	id serial primary key,
@@ -28,6 +27,13 @@ insert into employees (employee_name)
 ('Anthony'), ('Matthew'), ('Peter');
 
 
+
+/*	Таблица salary
+3) Создать таблицу salary
+- id. Serial  primary key,
+- monthly_salary. Int, not null
+4) Наполнить таблицу salary 15 строками:
+*/
 create table salary(
 	id serial primary key,
 	monthly_salary int not null
@@ -40,8 +46,17 @@ insert into salary (monthly_salary)
 	(1000), (1100), (1200), (1300), (1400),
 	(1500), (1600), (1700), (1800), (1900),
 	(2000), (2100), (2200), (2300), (2400), (2500);
-	
 
+
+
+/*	Таблица employee_salary
+5) Создать таблицу employee_salary
+- id. Serial  primary key,
+- employee_id. Int, not null, unique
+- salary_id. Int, not null
+6) Наполнить таблицу employee_salary 40 строками:
+- в 10 строк из 40 вставить несуществующие employee_id
+*/
 create table employee_salary(
 	id serial primary key,
 	employee_id int unique not null,
@@ -62,6 +77,14 @@ insert into employee_salary (employee_id, salary_id)
 	(76, (random()*(16-1)+1)), (77, (random()*(16-1)+1)), (78, (random()*(16-1)+1)), (79, (random()*(16-1)+1)), (80, (random()*(16-1)+1));
 	
 
+
+/*	Таблица roles
+7) Создать таблицу roles
+- id. Serial  primary key,
+- role_name. int, not null, unique
+8) Поменять тип столба role_name с int на varchar(30)
+9) Наполнить таблицу roles 20 строками:
+*/
 create table roles(
 	id serial primary key,
 	role_name int unique not null
@@ -95,6 +118,14 @@ insert into roles (role_name)
 	('Senior Automation QA engineer');
 	
 
+
+/*	Таблица roles_employee
+10) Создать таблицу roles_employee
+- id. Serial  primary key,
+- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+11) Наполнить таблицу roles_employee 40 строками:
+*/
 create table roles_employee(
 	id serial primary key,
 	employee_id int unique not null,
